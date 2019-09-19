0d7705264b91cd93f06b3d2f038d119ac40bd0fe
Vadim <vadim.dobrovolsky@gmail.com>
2019-09-19 18:52:19 +0000

    finalizing dependencies setup

--- code/setup.sh
+++ code/setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+
+# Cleaning work dir
+WORK_DIR=$1
+cd $WORK_DIR
+rm -rf $WORK_DIR/Mask_RCNN
+rm -rf $WORK_DIR/coco
+rm -r $WORK_DIR/mask_rcnn_coco.h5
+
 # Download and install Mask_RCNN implementations
 git clone https://github.com/matterport/Mask_RCNN
-cd Mask_RCNN
+cd $WORK_DIR/Mask_RCNN
 python setup.py install
 
 # Download pretrained model weights for COCO dataset
-wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
+wget -P $WORK_DIR https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
     
 # Install pycocotools to work with MS COCO data set. 
+cd $WORK_DIR
 git clone https://github.com/pdollar/coco.git
-cd coco/PythonAPI
+cd $WORK_DIR/coco/PythonAPI
 make
 sudo make install
 sudo python setup.py install
